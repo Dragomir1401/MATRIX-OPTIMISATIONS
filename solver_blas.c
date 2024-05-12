4bfe70f90fe47da47abbd04b12299adc331b44bf
Dragomir Andrei <andreidrago14@yahoo.com>
2024-05-12 18:34:28 +0300

    fixed blas

--- solver_blas.c
+++ solver_blas.c
@@ -2,7 +2,6 @@
 #include "cblas.h"
 
 // C = (At × B + B × A) × Bt
-
 double* my_solver(int N, double *A, double *B) {
     printf("BLAS SOLVER\n");
 
@@ -12,19 +11,16 @@ double* my_solver(int N, double *A, double *B) {
     double *AtxB_plus_BxA = calloc(N * N, sizeof(double));
 
     // Calculate A^T * B where A^T is lower triangular
-    // Here we use a temporary storage to hold the result of A^T * B
     cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1.0, A, N, B, N, 0.0, AtxB, N);
 
     // Calculate B * A where A is upper triangular
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0, B, N, A, N, 0.0, BxA, N);
 
-    // Sum AtxB and BxA
-    for (int i = 0; i < N * N; i++) {
-        AtxB_plus_BxA[i] = AtxB[i] + BxA[i];
-    }
+    // Sum AtxB and BxA using cblas_daxpy
+    cblas_daxpy(N * N, 1.0, AtxB, 1, BxA, 1); // BxA := 1.0 * AtxB + BxA
 
-    // Calculate (AtxB + BxA) * B^T
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1.0, AtxB_plus_BxA, N, B, N, 0.0, C, N);
+    // Calculate (AtxB + BxA) * B^T using BxA which now holds the sum of AtxB and BxA
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1.0, BxA, N, B, N, 0.0, C, N);
 
     free(AtxB);
     free(BxA);
